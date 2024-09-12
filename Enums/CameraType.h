6d06ac4ac8daadf9b50fc10bb954c46ebf3856bf
lepesh <nelepesh100@mail.ru>
2024-09-12 15:03:39 +0300

    Fix CmakeLists.txt

---
+++ Enums/CameraType.h
@@ -0,0 +1,12 @@
+//
+// Created by Lepesh on 12.09.2024.
+//
+
+#ifndef OPENGLPROJECT_CAMERATYPE_H
+#define OPENGLPROJECT_CAMERATYPE_H
+
+enum class CameraType{
+    ArcBallCamera,
+    FPSCamera
+};
+#endif //OPENGLPROJECT_CAMERATYPE_H
